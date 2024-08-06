b9b5196f4cb2b9e21ecc03ea86616d96064ac4e3
Jonas Brahmst <jonasbrahmst@gmail.com>
2024-08-06 19:49:35 +0200

    Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,30 @@ import NavBar from "@/components/navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://jonasbrahmst.vercel.app"),
+  keywords: [
+    "Jonas Brahmst",
+    "Webdesign",
+    "Webentwicklung",
+    "Frontend Entwicklung",
+    "Backend Entwicklung",
+    "Portfolio",
+    "Next.js",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Node.js",
+    "HTML",
+    "CSS",
+    "Responsive Design",
+    "UI/UX Design",
+    "SEO",
+    "Web Applikationen",
+    "Web Projekte",
+    "Moderne Webentwicklung",
+    "JavaScript Entwickler",
+    "Full Stack Entwickler"
+  ],
   title: "Jonas Brahmst | Portfolio",
   description: "Hello and welcome to my portfolio!",
 };
