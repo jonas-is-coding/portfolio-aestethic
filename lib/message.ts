64a66f589c09ec165145185bd3d901fb1c7fc3e6
jonas-is-coding <jonasbrahmst@gmail.com>
2024-09-11 14:19:39 +0200

    Commit

--- lib/message.ts
+++ lib/message.ts
@@ -1,24 +1,21 @@
-import { Twilio } from 'twilio';
+require("dotenv").config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = new Twilio(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
 export async function sendMessage(message: string, imageUrl?: string) {
-  const from = 'whatsapp:+14155238886';
-  const to = `whatsapp:${process.env.PHONE_NUMBER}`;
-
   if (imageUrl) {
-    await client.messages.create({
-      from,
-      to,
+    client.messages.create({
+      from: "whatsapp:+14155238886",
       body: message,
+      to: `whatsapp:${process.env.PHONE_NUMBER}`,
       mediaUrl: [imageUrl],
     });
   } else {
     await client.messages.create({
-      from,
-      to,
+      from: "whatsapp:+14155238886",
+      to: `whatsapp:${process.env.PHONE_NUMBER}`,
       body: message,
     });
   }
