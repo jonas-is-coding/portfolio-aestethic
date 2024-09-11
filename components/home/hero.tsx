e034d1b8fb2f4b1ab9e7d9f0024bd5fca59a15bc
jonas-is-coding <jonasbrahmst@gmail.com>
2024-09-11 18:36:24 +0200

    Commit

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,9 +1,7 @@
-import Other_Tip from "../content/other_tip";
-
 const HeroSection = () => {
   return (
     <main className="h-screen w-full flex flex-col items-center justify-center relative">
-      <Other_Tip />
+      
     </main>
   );
 };
