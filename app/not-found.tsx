fa314df5451366941d7e371ccdf6ecf3e6ca05fd
jonas-is-coding <jonasbrahmst@gmail.com>
2024-09-11 13:37:44 +0200

    Commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,7 +15,7 @@ const NotFound = () => {
       <Image src={"/404-ghost.png"} alt="404 Ghost" width={350} height={0} />
       <ul className="flex flex-col items-start justify-center gap-2">
         <h3 className="text-9xl font-semibold text-start">404</h3>
-        <p className="text-start opacity-60">We couldn't find the page you were looking for!</p>
+        <p className="text-start opacity-60">We couldnt find the page you were looking for!</p>
         <button
           onClick={handleGoBack}
           className="px-7 h-12 bg-wheat text-black rounded-full mt-10"
