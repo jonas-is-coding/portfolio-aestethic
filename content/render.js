33d7e9fc3c436ce8652fa87c61f7e44b56b1ab86
jonas-is-coding <jonasbrahmst@gmail.com>
2024-09-12 07:47:26 +0200

    Commit

--- content/render.js
+++ content/render.js
@@ -1,32 +1,28 @@
-const chromium = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 const fs = require("fs");
 const path = require("path");
 const { setTimeout } = require("node:timers/promises");
-
 async function renderComponent(partNumber, componentFile) {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
+  const browser = await puppeteer.launch({ 
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
-
   const page = await browser.newPage();
+
   await page.setViewport({ width: 1080, height: 1350 });
 
   await page.goto(
     "https://jonasbrahmst.vercel.app/screenshot/" +
-    componentFile +
-    "/" +
-    partNumber
-  );
+      componentFile +
+      "/" +
+      partNumber
+  ); 
 
   await setTimeout(3000);
 
-  const screenshotsDir = path.join('/tmp', "screenshots");
+  const screenshotsDir = path.join(__dirname, "screenshots");
   if (!fs.existsSync(screenshotsDir)) {
-    fs.mkdirSync(screenshotsDir, { recursive: true });
+    fs.mkdirSync(screenshotsDir);
   }
 
   const screenshotPath = path.join(
@@ -34,16 +30,13 @@ async function renderComponent(partNumber, componentFile) {
     `${componentFile}_part_${partNumber}.png`
   );
   await page.screenshot({ path: screenshotPath });
-
   await browser.close();
   return screenshotPath;
 }
-
 const partNumber = process.argv[2];
 const componentFile = process.argv[3];
-
 renderComponent(partNumber, componentFile)
   .then((screenshotPath) =>
     console.log(`Screenshot gespeichert unter: ${screenshotPath}`)
   )
-  .catch((err) => console.error("Fehler beim Rendern:", err));
+.catch((err) => console.error("Fehler beim Rendern:", err));
