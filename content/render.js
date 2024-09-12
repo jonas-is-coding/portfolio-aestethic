273b48db3a7ae8ef2b4292506cdb9b95bb201416
jonas-is-coding <jonasbrahmst@gmail.com>
2024-09-12 02:58:10 +0200

    Commit

--- content/render.js
+++ content/render.js
@@ -1,30 +1,35 @@
-const puppeteer = require("puppeteer-core");
+const chromium = require('chrome-aws-lambda');
 const fs = require("fs");
 const path = require("path");
 const { setTimeout } = require("node:timers/promises");
 
 async function renderComponent(partNumber, componentFile) {
-  const browser = await puppeteer.launch({ headless: true});
-  const page = await browser.newPage();
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
 
+  const page = await browser.newPage();
   // Setze die Viewport-Größe auf die Größe deiner Komponenten
   await page.setViewport({ width: 1080, height: 1350 });
 
   // Lade die React-App
   await page.goto(
     "https://jonasbrahmst.vercel.app/screenshot/" +
-      componentFile +
-      "/" +
-      partNumber
+    componentFile +
+    "/" +
+    partNumber
   ); // Stelle sicher, dass die URL korrekt ist
 
   // Warte, bis die Seite geladen ist
   await setTimeout(3000);
 
   // Erstelle das Verzeichnis, falls es nicht existiert
-  const screenshotsDir = path.join(__dirname, "screenshots");
+  const screenshotsDir = path.join('/tmp', "screenshots");
   if (!fs.existsSync(screenshotsDir)) {
-    fs.mkdirSync(screenshotsDir);
+    fs.mkdirSync(screenshotsDir, { recursive: true });
   }
 
   // Nimm einen Screenshot des gesamten Inhalts, der dem Viewport entspricht
